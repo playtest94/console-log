c8b7e30e95f61c04fe0aae6a3d418285053a5d93
Pedro Diaz <pdiaz.btm@gmail.com>
2024-05-20 21:11:11 -0400

    Fix locations logs

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ app.get('/', (req, res) => {
     return res.json({ message: "ok" })
 })
 
-app.post('/log', (req, res) => {
+app.post('/location', (req, res) => {
 
+    if (req.body.name === "location") {
+        const loc = req?.body?.location
+        console.log(`${loc.createdAt} ${loc.uuid.substring(loc.uuid.length - 8)} ${loc.isMoving ? "moving" : "stationary"} ${loc.appState}`)
+    }
+    return res.json({ message: "ok" })
+})
+
+app.post('/log', (req, res) => {
     console.log("<===>", req?.body?.name)
-    // if (req.body.name === "location") {
-    //     const loc = req?.body?.location
-    //     console.log(`${loc.createdAt} ${loc.uuid.substring(loc.uuid.length - 8)} ${loc.isMoving ? "moving" : "stationary"} ${loc.appState}`)
-    // } else {
-    //     console.log("<===>", req?.body)
-    // }
     return res.json({ message: "ok" })
 })
 
